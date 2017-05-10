d26fc7087fe3e2c8a715393eb77545eeabf6a787
Raul Martinez i Peris <raul@cuchame.es>
2017-05-10 10:32:48 +0200

    Added environment configuration

---
+++ environment_configuration.rb
@@ -0,0 +1,21 @@
+def retrieve_mode
+  begin
+    consensus_environment = ENV.fetch('CONSENSUS_MODE')
+  rescue
+    consensus_environment = nil
+  end
+  return consensus_environment
+end
+
+def retrieve_port
+  begin
+    capybara_default_port = eval File.open('.env').read
+  rescue
+    capybara_default_port = '4567'
+  end
+  return capybara_default_port
+end
+
+def retrieve_travis
+  File.exist?('travis.ci')
+end
