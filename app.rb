a90fe9138849dae68d446809489b8e8a7d8bffc7
paconabel <paconabel@gmail.com>
2017-05-11 17:22:30 +0200

    Add home route html

--- app.rb
+++ app.rb
@@ -5,7 +5,12 @@ class App < Sinatra::Base
   set :public_folder, 'public/'
   enable :static
 
+  get '/' do
+    File.read(File.join('public', 'home.html'))
+  end
+
   get '/home' do
     File.read(File.join('public', 'home.html'))
   end
+  
 end
