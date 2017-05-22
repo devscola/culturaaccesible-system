346470474cdc53387bd35da11ebd853a986f4dba
miguelonga <miguelmundiaragones@gmail.com>
2017-05-22 12:45:57 +0200

    refactor

--- public/js/pages/info.js
+++ public/js/pages/info.js
@@ -2,8 +2,6 @@ Class('Page.Info', {
     initialize: function() {
         this.publish();
         new Info.Form();
-        new Info.Textarea();
-        new Info.Field();
     },
 
     publish: function() {
