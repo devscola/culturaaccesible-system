9fe3686b0661e75197c44016b0fc43bd12211db6
miguelonga <miguelmundiaragones@gmail.com>
2017-05-22 13:04:01 +0200

    info form submit on save

--- public/js/components/info/form.js
+++ public/js/components/info/form.js
@@ -6,6 +6,11 @@ Class('Info.Form', {
         Info.Form.Super.call(this, 'info-form');
         this.field = document.getElementById('info-field');
         this.textarea = document.getElementById('info-textarea');
+        this.element.addEventListener('submitted', this.saveInfo.bind(this));
+    },
+
+    saveInfo: function(info) {
+        Bus.publish('info.retrieved', info);
     },
 
     subscribe: function() {
