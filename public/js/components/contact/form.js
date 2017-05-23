e14d7c4b9c50c8fbb96ef2f19d5f7489c5c0ec03
Nacho <nandawtek@gmail.com>
2017-05-23 11:05:16 +0200

    Unify Contact & Form WebComponent

--- public/js/components/contact/form.js
+++ public/js/components/contact/form.js
@@ -1,23 +1,38 @@
-Class('Contact.Form', {
+Class('Museum.Contact', {
 
     Extends: Component,
 
     fields: [{"label" : "Phone"}, {"label" : "Email"}, {"label" : "Web"}],
 
+    view: document.getElementById('result'),
+
     initialize: function() {
-        Contact.Form.Super.call(this, 'formulary');
+        Museum.Contact.Super.call(this, 'formulary');
         this.element.fields = this.fields;
         this.element.addEventListener('buttonToggled', this.toggleButton.bind(this));
         this.element.addEventListener('submitted', this.renderContact.bind(this));
     },
 
     toggleButton: function(hasContent) {
-      this.element.disableSave = !hasContent;
+        this.element.disableSave = !hasContent;
+    },
+
+    render: function(contactInfo) {
+        this.view.contact = contactInfo;
+        this.show();
+    },
+
+    show: function() {
+        this.view.visible = true;
     },
 
     renderContact: function(contactInfo) {
-      Bus.publish('render.contact', contactInfo.detail);
+        Bus.publish('render.contact', contactInfo.detail);
+    },
+
+    subscribe: function() {
+        Bus.subscribe('render.contact', this.render.bind(this));
     }
 
-});
 
+});
