1cb491218a4abd6c047785681668d0845643f00e
Fran <diazgomezfrancisco@gmal.com>
2017-05-26 16:45:46 +0200

    new Add button

--- public/js/components/contact/form.js
+++ public/js/components/contact/form.js
@@ -2,8 +2,15 @@ Class('Museum.Contact', {
 
     Extends: Component,
 
+    contactDetail: [
+        {type: "phone", label: "Phone number"},
+        {type: "email", label: "Email"},
+        {type: "web", label: "Website"}
+    ],
+
     initialize: function() {
-        Museum.Contact.Super.call(this, 'input-container');
+        Museum.Contact.Super.call(this, 'formulary');
+        this.element.contactDetail = this.contactDetail;
     }
 
 });
