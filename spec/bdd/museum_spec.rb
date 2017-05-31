f9fa63e787c3e68a5958f49c3cd78412b262797b
Manel Romero <manel@manelromero.com>
2017-05-31 17:24:38 +0200

    Comment test

--- spec/bdd/museum_spec.rb
+++ spec/bdd/museum_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper_bdd'
 require_relative 'test_support/fixture_museum'
 require_relative 'test_support/museum'
 
-feature 'Museum' do
+xfeature 'Museum' do
   scenario 'allows submit when enough content', :wip do
     current = Fixture::Museum.enough_content
     expect(current.save_activated?).to be true
