f1ca36889730d324bdf6a382698b92f22590f0b6
Raul Martinez i Peris <raul@cuchame.es>
2017-09-17 14:15:39 +0200

    Fixed bug in empty hours

--- public/js/components/museum/panel.js
+++ public/js/components/museum/panel.js
@@ -9,7 +9,25 @@ Class('Museum.View', {
     render: function(museumData) {
         this.element.museumData = museumData;
         this.element.visibility = 'show';
+        this.goToInfoPanel();
         Bus.publish('museum.list.retrieve');
+    },
+
+    goToInfoPanel: function() {
+        var museumId = this.loadShortUrlData(3);
+        window.location = '/museum/' + museumId ;
+    },
+
+    getMuseumId: function() {
+      var url = window.location.href;
+      return this.loadShortUrlData(3);
+    },
+
+    loadShortUrlData: function(index) {
+        var urlString = window.location.href;
+        var regexp = /\/(museum)(\/)(.*)(\/)(edit)/;
+        var data = regexp.exec(urlString)[index];
+        return data;
     },
 
     subscribe: function() {
