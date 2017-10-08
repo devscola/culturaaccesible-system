5a9a8f76e4bfc6dcb2dcde430ae18453a5abb3b2
NaNdaWteK <nandawtek@gmail.com>
2017-10-08 09:38:35 +0200

    refactor museum integration test

--- spec/integration/museum_routes_spec.rb
+++ spec/integration/museum_routes_spec.rb
@@ -14,71 +14,76 @@ describe 'Museum controller' do
     Museums::Repository.flush
   end
 
-  it 'retrieves all museums' do
-    add_museum
-    post '/api/museum/list'
-
-    result = parse_response
-    expect(result.any?).to be true
+  context 'creating' do
+    it 'stores museum' do
+      add_museum
+      first_museum_id = parse_response['id']
+      add_museum
+      second_museum_id = parse_response['id']
+  
+      expect(first_museum_id == second_museum_id).to be false
+    end
   end
 
-  it 'stores museum with id' do
-    add_museum
-    first_museum_id = parse_response['id']
+  context 'retrieving' do
+    it 'all museums' do
+      add_museum
 
-    add_museum
-    second_museum_id = parse_response['id']
+      post '/api/museum/list'
+      museums = parse_response
 
-    expect(first_museum_id == second_museum_id).to be false
+      expect(museums.any?).to be true
+    end
+
+    it 'museum' do
+      add_museum
+      museum_id = parse_response['id']
+      payload = { id: museum_id }.to_json
+  
+      post '/api/museum/retrieve', payload
+      museum = parse_response
+
+      expect(museum['info']['name']).to eq 'some name'
+      expect(museum['id']).to eq museum_id      
+    end
   end
 
-  it 'retrieve museum by id' do
-    add_museum
-    museum_id = parse_response['id']
-    payload = { id: museum_id }.to_json
-
-    post '/api/museum/retrieve', payload
-
-    museum_name = parse_response['info']['name']
-    expect(museum_name).to eq 'some name'
-  end
-
-  it 'updates museum returns updated phone and price data' do
-    old_name = 'some name'
-    new_museum_name = 'some updated name'
-
-    add_museum
-    museum_id = parse_response['id']
-
-    update_museum(museum_id)
-    updated_museum = parse_response
-
-    expect(museum_id).to eq updated_museum['id']
-    expect(new_museum_name).to eq updated_museum['info']['name']
-    expect(updated_museum['contact']['phone']).to eq ['123456789', '987654321']
-    expect(updated_museum['price']['general']).to eq ['25', '30']
+  context 'updating' do
+    it 'updates museum returns updated data' do
+      new_museum_name = 'some updated name'
+      add_museum
+      museum_id = parse_response['id']
+  
+      update_museum(museum_id)
+      updated_museum = parse_response
+  
+      expect(museum_id).to eq updated_museum['id']
+      expect(new_museum_name).to eq updated_museum['info']['name']
+      expect(updated_museum['contact']['phone']).to eq ['123456789', '987654321']
+      expect(updated_museum['price']['general']).to eq ['25', '30']
+    end
   end
 end
 
-  def add_museum
-    museum = {
-        info: {name: 'some name', description: 'some description'},
-        location: {street: 'some street'}
-      }.to_json
-    post '/api/museum/add', museum
-  end
+def add_museum
+  museum = {
+      info: {name: 'some name', description: 'some description'},
+      location: {street: 'some street'}
+    }.to_json
+  post '/api/museum/add', museum
+end
 
-  def update_museum(id)
-    museum = {
-        id: id,
-        info: {name: 'some updated name', description: 'some description'},
-        location: {street: 'some street'},
-        contact: {phone: ['123456789', '987654321']},
-        price: {general: ['25', '30']}
-      }.to_json
-    post 'api/museum/add', museum
-  end
+def update_museum(id)
+  museum = {
+      id: id,
+      info: {name: 'some updated name', description: 'some description'},
+      location: {street: 'some street'},
+      contact: {phone: ['123456789', '987654321']},
+      price: {general: ['25', '30']}
+    }.to_json
+  post 'api/museum/add', museum
+end
 
-  def parse_response
-    JSON.parse(last_response.body)
-  end
+def parse_response
+  JSON.parse(last_response.body)
+end
